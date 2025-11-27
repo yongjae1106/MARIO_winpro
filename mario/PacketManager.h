20eb041df0ac35deccaec69905e069c079e399da
hyunsuk01 <76142195+hyunsuk01@users.noreply.github.com>
2025-11-27 12:17:37 +0900

    PacketManager.h 버퍼 기반 패킷 처리 방식 적용

--- mario/PacketManager.h
+++ mario/PacketManager.h
@@ -1,20 +1,28 @@
 #pragma once
-#include "PacketInfo.h"
 #include <vector>
+#include "PacketInfo.h"
+#include "NetworkManager/ThreadSafeQueue.h"
+
+struct PacketData
+{
+    unsigned int type;
+    std::vector<char> data;
+};
 
 class PacketManager
 {
 public:
-    int TryParse(const std::vector<char>& buffer);
-    void HandlePacket(unsigned int type, const char* data, unsigned int length);
+    static PacketManager* GetInstance();
 
-    // 직렬화 (클라 → 서버)
+    // ---- Parsing ----
+    void ProcessReceivedData(std::vector<char>& buffer);
+    bool TryGetPacket(PacketData& outPacket);
+
+    // ---- Serialize (클라→서버) ----
     unsigned int Serialize_MOVE(char* buffer, const Packet_MOVE_C2S& move);
     unsigned int Serialize_ATTACK(char* buffer, const Packet_ATTACK_C2S& atk);
 
 private:
-    // 패킷 처리 함수 (게임 로직과 연동 예정)
-    void OnMove(const Packet_MOVE_S2C& pkt);
-    void OnHit(const Packet_HIT_S2C& pkt);
-    void OnBlock(const Packet_BLOCK_S2C& pkt);
+    PacketManager();
+    ThreadSafeQueue<PacketData> m_receivedPackets;
 };
