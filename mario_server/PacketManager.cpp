29c51fa663508738ca119aba4bd34215af48fe08
hyunsuk01 <76142195+hyunsuk01@users.noreply.github.com>
2025-12-02 12:27:25 +0900

    Move, Attack -> Input

--- mario_server/PacketManager.cpp
+++ mario_server/PacketManager.cpp
@@ -1,41 +1,59 @@
 #include "PacketManager.h"
 #include <cstring>
 #include <iostream>
+#include "GameWorld.h" // 角力 霸烙 包府 肺流
 
-int PacketManager::ProcessPacket(const std::vector<char>& buffer)
+int PacketManager::TryParse(std::vector<char>& buffer, unsigned int socketID, GameWorld* world)
 {
-    if (buffer.size() < sizeof(PacketHeader))
-        return 0;
+    while (true)
+    {
+        if (buffer.size() < sizeof(PacketHeader))
+            return 0;
 
-    PacketHeader header;
-    memcpy(&header, buffer.data(), sizeof(PacketHeader));
+        PacketHeader header;
+        memcpy(&header, buffer.data(), sizeof(PacketHeader));
 
-    if (buffer.size() < header.totalLength)
-        return 0;
+        if (buffer.size() < header.totalLength)
+            return 0;
 
-    HandlePacket(header.type, buffer.data() + sizeof(PacketHeader), header.totalLength);
+        HandlePacket(header.type, buffer.data() + sizeof(PacketHeader), header.totalLength, socketID, world);
 
-    return header.totalLength;
+        buffer.erase(buffer.begin(), buffer.begin() + header.totalLength);
+    }
+
+    return 0;
 }
 
-void PacketManager::HandlePacket(unsigned int type, const char* data, unsigned int length)
+void PacketManager::HandlePacket(unsigned int type, const char* data, unsigned int length, unsigned int socketID, GameWorld* world)
 {
     switch (type)
     {
-    case PKT_MOVE:
+    case PKT_INPUT:
     {
-        Packet_MOVE_C2S pkt;
+        Packet_INPUT_C2S pkt{};
         memcpy(&pkt, data, sizeof(pkt));
-        std::cout << "[Server] Player " << pkt.playerID << " moved to (" << pkt.x << ", " << pkt.y << ")\n";
+
+        switch (pkt.inputType)
+        {
+        case InputType::MOVE:
+            std::cout << "[Server] Player(" << socketID << ") MOVE => (" << pkt.x << "," << pkt.y << ")\n";
+            //world->OnPlayerMove(socketID, pkt.x, pkt.y, pkt.vx, pkt.vy, pkt.state);
+            break;
+
+        case InputType::ATTACK:
+            std::cout << "[Server] Player(" << socketID << ") ATTACK Target(" << pkt.targetID << ") Damage(" << pkt.damage << ")\n";
+            //world->OnPlayerAttack(socketID, pkt.targetID, pkt.damage);
+            break;
+
+        default:
+            std::cout << "[Server] Unknown InputType\n";
+            break;
+        }
         break;
     }
-    case PKT_ATTACK:
-    {
-        Packet_ATTACK_C2S pkt;
-        memcpy(&pkt, data, sizeof(pkt));
-        std::cout << "[Server] Player " << pkt.playerID << " attacked target " << pkt.targetID
-            << " with damage " << pkt.damage << "\n";
+
+    default:
+        std::cout << "[Server] Unknown Packet Type: " << type << "\n";
         break;
-    }
     }
 }
--- mario_server/PacketManager.h
+++ mario_server/PacketManager.h
@@ -2,18 +2,21 @@
 #include "PacketInfo.h"
 #include <vector>
 
-// 추가: GameWorld 전방 선언 (헤더 순환 참조 방지)
+// 전방 선언(순환 참조 방지)
 class GameWorld;
 
 class PacketManager
 {
 public:
-    // 수정: 인자에 GameWorld* world 추가
-    int TryParse(const std::vector<char>& buffer, unsigned int socketID, GameWorld* world);
+    // 클라이언트별 버퍼 기반 패킷 처리 (ThreadManager → ClientLoop 이용)
+    int TryParse(std::vector<char>& buffer, unsigned int socketID, GameWorld* world);
+
+private:
     void HandlePacket(unsigned int type, const char* data, unsigned int length, unsigned int socketID, GameWorld* world);
 
-    // 서버 → 클라 직렬화
-    unsigned int Serialize_MOVE(char* buffer, const Packet_MOVE_S2C& move);
+public:
+    // ---- Server → Client 직렬화 ----
+    unsigned int Serialize_PLAYER_STATE(char* buffer, const Packet_MOVE_S2C& move);
     unsigned int Serialize_HIT(char* buffer, const Packet_HIT_S2C& hit);
     unsigned int Serialize_BLOCK(char* buffer, const Packet_BLOCK_S2C& blk);
 };
